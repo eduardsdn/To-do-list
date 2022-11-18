4bdc8a0d8a5540b0acee17bbeea5e448009c16e3
eduardsdn <eduard.sdn@gmail.com>
2022-11-18 13:51:34 +0100

    added form event

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 //Task object constructor
 
-function Task(content, isComplete){
+function task(content){
     this.content = content;
-    this.isComplete = isComplete;
 }
+
+//list of all task objects
+tasks = []
+
+form = document.getElementById("form")
+form.addEventListener('submit', function(event){
+    event.preventDefault()
+
+    let content = document.getElementById('text-input').value
+
+    let taskObj = new task(content)
+    
+    tasks.push(taskObj)
+})
+
+
+
+
+
