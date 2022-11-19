e6066bdd2a4230a4395bca600e6aea3726bde471
eduardsdn <eduard.sdn@gmail.com>
2022-11-19 13:41:30 +0100

    added DOM manipulation for tasks

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-//Task object constructor
 
-function task(content){
+tasks = [] //list of all task objects
+
+function task(content){ //Task object constructor
     this.content = content;
 }
-
-//list of all task objects
-tasks = []
 
 form = document.getElementById("form")
 form.addEventListener('submit', function(event){
@@ -16,7 +14,33 @@ form.addEventListener('submit', function(event){
     let taskObj = new task(content)
     
     tasks.push(taskObj)
+
+    console.log(tasks)
+
+    displayTasks(tasks)
 })
+
+function displayTasks(tasks){
+    for(let i = 0; i < tasks.length; i++) {
+        let newTask = document.createElement("li")
+        newTask.classList.add("task")
+        
+        let newTaskContent = `<div class="text-with-tick">
+                                <input type="radio">
+                                <p>${tasks[i].content}</p>
+                              </div>
+
+                              <button class="delete-note-btn">x</button>`
+
+        newTask.innerHTML = newTaskContent
+
+        tasksContainer = document.querySelector(".list-holder")
+        
+        // tasksContainer.appendChild(newTask)
+    }
+}
+
+
 
 
 
